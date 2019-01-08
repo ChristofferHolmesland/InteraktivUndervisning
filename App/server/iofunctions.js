f3cc74280cc2ceeb6986649e964ad712ea903625
christofferholmesland <cr.holmesland@stud.uis.no>
2019-01-08 16:42:39 +0100

    socketio in seperate file

---
+++ App/server/iofunctions.js
@@ -0,0 +1,46 @@
+var socketio = require('socket.io');
+
+roommap = "";
+
+module.exports.listen = function(server) {
+    io = socketio.listen(server);
+
+    io.on("connection", (socket) => {
+        console.log(`Socket connected with socket id: ${socket.id}`);
+        console.log(socket);
+
+        socket.on("sendAnswer", (data) => {
+            if (socket.rooms.indexOf(data.roomkey) < 0) {
+                return;
+            }
+
+            roommap.get(data.roomkey).addAnswer(data);
+        });
+
+        socket.on("clientStarted", () => {
+            console.log("Client connected");
+            socket.emit("clientResponse");
+        });
+    
+        socket.on("hostStarted", () => {
+            console.log("Host connected");
+            socket.emit("hostResponse");
+        });
+    
+        socket.on("clientRequest", (msg) => {
+            console.log("got this message from client: " + msg);
+            console.log(socket.id);
+        });
+    
+        socket.on("hostRequest", (msg) => {
+            console.log("got this message from host: " + msg);
+        });
+        
+        socket.on('disconnect', function(){
+            console.log(`Socket disconnected with socket id: ${socket.id}`);
+        });
+            
+    });
+
+    return io;
+}
