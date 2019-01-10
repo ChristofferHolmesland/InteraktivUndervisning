6b2298605b00d8244567b3d24ff5ef186b6d52fb
gauhau <35257608+gauhau@users.noreply.github.com>
2019-01-10 16:09:16 +0100

    Added some bootstrap and cleaned up some files

--- App/server/public/js/chunk-2d0e95df.e4659b0a.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e95df"],{"8cdb":function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"404"},[n("h1",[e._v("This is the 404 page")])])}],i=n("2877"),l={},u=Object(i["a"])(l,s,c,!1,null,null,null);u.options.__file="404.vue";t["default"]=u.exports}}]);
-//# sourceMappingURL=chunk-2d0e95df.e4659b0a.js.map
