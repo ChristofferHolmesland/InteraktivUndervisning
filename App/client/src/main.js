a0681b08769f47cdafb278cbd5baf8fff67ec69c
Gaute Haugen <gaute.haugen@lyse.net>
2019-01-18 16:08:25 +0100

    Implemented vuex. Implemented pwa support. Moved config files from package.json to their own files. Made the logo

--- App/client/src/main.js
+++ App/client/src/main.js
@@ -1,32 +1,29 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import io from "socket.io-client";
+//import socketio from 'socket.io-client';
+import VueSocketIO from 'vue-socket.io';
+
+//export const SocketInstance = socketio("http://localhost:80");
 
 Vue.use(BootstrapVue);
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: "http://localhost:80",
+  vuex: {
+      store
+  }
+}));
 
 Vue.config.productionTip = false;
 
-export const dataBus = new Vue();
-
-dataBus.socket = io("localhost:80");
-dataBus.locale = {};
-dataBus.localeList = [];
-dataBus.localeLoaded = false;
-
-dataBus.socket.emit("getLocaleRequest", "no");
-
-dataBus.socket.on("getLocaleResponse", function(data) {
-	dataBus.locale = data.locale;
-	dataBus.localeList = data.localeList;
-	dataBus.localeLoaded = true;
-	dataBus.$emit("localeLoaded");
-});
-
 new Vue({
-	router,
-	render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount("#app");
