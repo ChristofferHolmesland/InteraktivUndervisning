714189c9f487ee2359cc36725390fa8c5bb10882
Gaute Haugen <35257608+gauhau@users.noreply.github.com>
2019-01-24 14:36:30 +0100

    Quiz (#38)
    * Started work on admin pages
    
    * Created admim pages: Quiz, Questions, Sessions
    
    * admin questions
    
    * admin questions2
    
    * Questions can now be added on the questions page
    
    * Seperate modal
    
    * trying to show edit modal for each question in the list
    
    * progresszzzzzzzzzzzzz
    
    * removed old files
    
    * started the session page
    
    * dfølkn
    
    * finished logic for session
    
    * added quiz screen and waitingroom screen
    
    * test

--- App/client/src/components/admin/Quiz.vue
+++ App/client/src/components/admin/Quiz.vue
@@ -1,12 +1,37 @@
 <template>
-	<div class="admin">
-		<h1>This is the quiz page</h1>
+	<div class="quiz">
+		<b-list-group>
+			<b-list-group-item 	v-for="session in sessionList"
+								:key="session.id">
+				{{session.title}} 	<b-button :id="session.id" 
+											@click="initializeQuiz($event)">
+										Start
+									</b-button>
+			</b-list-group-item>
+		</b-list-group>
 	</div>
 </template>
 
 <script>
 	export default {
-        name: 'Quiz',
+		name: 'quiz',
+		data() {
+			return {
+				sessionList: [{id: "3434", title: "quiz 1"}]
+			}
+		},
+		sockets: {
+			initializeQuizResponse(quizId) {
+				console.log(quizId)
+				this.$router.push(`/admin/quiz/${quizId}`);
+			}
+		},
+		methods: {
+			initializeQuiz(event){
+				console.log(event.target.id)
+				this.$socket.emit("initializeQuiz", event.target.id);
+			}
+		},
     }
 
 </script>
