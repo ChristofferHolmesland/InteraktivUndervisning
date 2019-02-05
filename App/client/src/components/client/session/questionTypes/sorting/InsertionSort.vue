06b0dc38d8d62dca7445bad4cfa4f08e02960772
Lupu2 <35259974+Lupu2@users.noreply.github.com>
2019-02-05 20:26:55 +0100

    Added InsertionSort And ShellSort with unitest's

---
+++ App/client/src/components/client/session/questionTypes/sorting/InsertionSort.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="array">
+
+    </div>
+</template>
+
+<script>
+	export default {
+		name: "array"
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ App/client/src/components/client/session/questionTypes/sorting/ShellSort.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+	export default {
+		name: "ShellSort"
+	}
+</script>
+
+<style scoped>
+
+</style>
