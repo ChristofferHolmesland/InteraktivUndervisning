4fbc842763166f87fc28dbca09138140c3000700
Christoffer Holmesland <ChristofferHolmesland@users.noreply.github.com>
2019-02-08 09:45:34 +0100

    Implmented graphdrawer config as a json object

--- Prototyper/htmlGraph/main.js
+++ Prototyper/htmlGraph/main.js
@@ -1,2 +1,11 @@
 let c = document.getElementById("canvas");
-let graphDrawer = new GraphDrawer(c);
+let graphDrawer = new GraphDrawer(c, {
+    nodeShape: "Circle",
+    controlType: "Graph0",
+    quicksort: {
+        bsf: 2.5,
+        pivotColor: "#add8e6",
+        selectedColor: "red",
+        sortType: "vSorter"
+    }
+});
