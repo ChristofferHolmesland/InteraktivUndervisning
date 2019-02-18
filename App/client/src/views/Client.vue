ba9b4cef3a34e30b085c8b923f297e3f216081db
christofferholmesland <cr.holmesland@stud.uis.no>
2019-02-18 12:58:16 +0100

    Merge/Quick sort can now be used in presentation mode, stepping

--- App/client/src/views/Client.vue
+++ App/client/src/views/Client.vue
@@ -7,7 +7,11 @@
 					<JoinSession/>
 				</b-col>
 				<b-col cols="12" lg="8">
-					<GraphDrawer controlType="Sort" sortType="Mergesort" :steps="steps" />
+					<GraphDrawer 
+								controlType="Sort" 
+								sortType="Quicksort" 
+								operatingMode="Presentation" 
+								:steps="steps" />
 				</b-col>
 			</b-row>
 		</b-container>
@@ -27,70 +31,40 @@ export default {
 	data() {
 		return {
 			steps: [
-				{
-					type: "Initial",
-					list: [10, 9, 2, 3, 11, 7, 5],
-				},
-				{ 
-					type: 'Split',
-  					list: [ 10, 9, 2, 3, 11, 7, 5 ],
-  					left: [ 10, 9, 2, 3 ],
-					  right: [ 11, 7, 5 ] 
-				},
-				{ 
-					type: 'Split',
-					list: [ 10, 9, 2, 3 ],
-					left: [ 10, 9 ],
-					right: [ 2, 3 ] 
-				},
-				{ 
-					type: 'Split', 
-					list: [ 10, 9 ], 
-					left: [ 10 ], 
-					right: [ 9 ] 
-				},
-				{ 
-					type: 'Merge', 
-					list1: [ 10 ], 
-					list2: [ 9 ], 
-					merged: [ 9, 10 ]
-				},
-				{ 
-					type: 'Split', 
-					list: [ 2, 3 ], 
-					left: [ 2 ], 
-					right: [ 3 ] 
-				},
-				{ 
-					type: 'Merge', 
-					list1: [ 2 ], 
-					list2: [ 3 ], 
-					merged: [ 2, 3 ] 
-				},
-				{ 
-					type: 'Merge',
-					list1: [ 9, 10 ],
-					list2: [ 2, 3 ],
-					merged: [ 2, 3, 9, 10 ]
-				},
-				{ 
-					type: 'Split',
-					list: [ 11, 7, 5 ],
-					left: [ 11, 7 ],
-					right: [ 5 ]
-				},
-				{ 
-					type: 'Split', list: [ 11, 7 ], left: [ 11 ], right: [ 7 ] },
-				{ type: 'Merge', list1: [ 11 ], list2: [ 7 ], merged: [ 7, 11 ] },
-				{ type: 'Merge',
-				list1: [ 7, 11 ],
-				list2: [ 5 ],
-				merged: [ 5, 7, 11 ] },
-				{ type: 'Merge',
-				list1: [ 2, 3, 9, 10 ],
-				list2: [ 5, 7, 11 ],
-				merged: [ 2, 3, 5, 7, 9, 10, 11 ] }
-			]
+    {
+        type: "Initial",
+        list: [10, 9, 2, 3, 11, 7, 5],
+    },
+    {
+        type: "Split",
+        pivot: 5,
+        list: [10, 9, 2, 3, 11, 7, 5],
+        left: [2, 3],
+        right: [10, 9, 11, 7],
+    },
+    {
+        type: "Split",
+        pivot: 2,
+        list: [2, 3],
+        left: [3],
+        right: [],
+    },
+    {
+        type: "Split",
+        pivot: 10,
+        list: [10, 9, 11, 7],
+        left: [7, 9],
+        right: [11],
+    },
+    {
+        type: "Split",
+        pivot: 7,
+        list: [7, 9],
+        left: [9],
+        right: []
+    }
+]
+
 		}
 	},
 	created() {
