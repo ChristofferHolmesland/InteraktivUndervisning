d040f78e7f20c0440c77bd757fee72ce9c9d5420
christofferholmesland <cr.holmesland@stud.uis.no>
2019-02-20 14:32:03 +0100

    Implemented the djikstra algorithm

--- App/server/js/algorithms/graphs/djikstra.js
+++ App/server/js/algorithms/graphs/djikstra.js
@@ -1,7 +1,11 @@
 /*
     Stepping implementation of the djikstra algorithm.
+
+    Step definitions:
+        - Giving a node a new distance.
+        - The shortest path from "from" to "to".
 */
-function d(graph, from, to) {
+module.exports.djikstra = function(graph, from, to) {
     let currentStep = 0;
     let steps = [
         {
@@ -13,11 +17,91 @@ function d(graph, from, to) {
     ];
 
     let djikstra = () => {
+        let unvisited = [];
 
+        for (let i = 0; i < graph.nodes.length; i++) {
+            let node = graph.nodes[i];
+            node.visited = false;
+            node.distance = Infinity;
+            unvisited.push(node);
+        }
+
+        let current = from;
+        current.distance = undefined;
+
+        do {
+            if (current == to) {
+                let path = [];
+                path.push(current);
+                
+                current = current.previous;
+                while (current !== undefined) {
+                    path.unshift(current);
+                    current = current.previous;
+                }
+
+                steps.push({
+                    type: "Path",
+                    path: path
+                });
+
+                break;
+            }
+
+            for (let i = 0; i < graph.edges.length; i++) {
+                let edge = graph.edges[i];
+                if (edge.n1 == current) {
+                    if (edge.n1.visited) continue;
+
+                    let distance = edge.v;
+                    if (current.distance) distance += current.distance;
+
+                    if (distance < edge.n2.distance) {
+                        edge.n2.distance = distance;
+                        edge.n2.previous = current;
+                        steps.push({
+                            type: "Distance",
+                            current: current,
+                            node: edge.n2
+                        });
+                    }
+                } else if (edge.n2 == current && !graph.directed) {
+                    if (edge.n2.visited) continue;
+
+                    let distance = edge.v;
+                    if (current.distance) distance += current.distance;
+
+                    if (distance < edge.n1.distance) {
+                        edge.n1.distance = distance;
+                        edge.n1.previous = current;
+                        steps.push({
+                            type: "Distance",
+                            current: current,
+                            node: edge.n1
+                        });
+                    }
+                }
+            }
+
+            current.visited = true;
+            unvisited.splice(unvisited.indexOf(current), 1);
+            
+            let nodeIndex = 0
+            let minDistance = unvisited[0].distance; 
+            for (let i = 1; i < unvisited.length; i++) {
+                if (unvisited[i].distance < minDistance) {
+                    nodeIndex = i;
+                    minDistance = unvisited[i].distance;
+                }
+            }
+            current = unvisited[nodeIndex];
+        } while (unvisited.length > 0);
     }
 
+    djikstra();
+
     return {
-        isSorted: function() {
+        isDone: function() {
             return currentStep == steps.length - 1;
         },
         get: function() {
@@ -44,4 +128,72 @@ function d(graph, from, to) {
     }
 };
 
-console.log(d(graph).reset());
+
+/* Test data
+let n1 = { v: 1 };
+let n2 = { v: 2 };
+let n3 = { v: 3 };
+let n4 = { v: 4 };
+let n5 = { v: 5 };
+let n6 = { v: 6 };
+
+let edges = [
+    {
+        n1: n1,
+        n2: n2,
+        v: 7
+    },
+    {
+        n1: n1,
+        n2: n6,
+        v: 14
+    },
+    {
+        n1: n1,
+        n2: n3,
+        v: 9
+    },
+    {
+        n1: n2,
+        n2: n3,
+        v: 10
+    },
+    {
+        n1: n2,
+        n2: n4,
+        v: 15
+    },
+    {
+        n1: n3,
+        n2: n6,
+        v: 2
+    },
+    {
+        n1: n3,
+        n2: n4,
+        v: 11
+    },
+    {
+        n1: n6,
+        n2: n5,
+        v: 9
+    },
+    {
+        n1: n4,
+        n2: n5,
+        v: 6
+    }
+]
+
+let graph = {
+    directed: false,
+    nodes: [n1, n2, n3, n4, n5, n6],
+    edges: edges
+};
+
+let stepper = d(graph, n1, n5);
+console.log(stepper.get());
+
+while (!stepper.isDone()) {
+    console.log(stepper.step());
+} */
