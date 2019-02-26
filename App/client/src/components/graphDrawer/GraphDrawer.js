ebccd7904e6cabd14866b5a50ee9a1cacaa97988
christofferholmesland <cr.holmesland@stud.uis.no>
2019-02-26 08:37:10 +0100

    Formatting

--- App/client/src/components/graphDrawer/GraphDrawer.js
+++ App/client/src/components/graphDrawer/GraphDrawer.js
@@ -552,15 +552,22 @@ export default class GraphDrawer {
 
 	// ref: https://stackoverflow.com/a/1501725, 20.02.2019
 	_distToSegmentSquared(p, v, w) {
-		function sqr(x) { return x * x }
-		function dist2(v, w) { return sqr(v.x - w.x) + sqr(v.y - w.y) }
+		function sqr(x) {
+			return x * x;
+		}
+
+		function dist2(v, w) {
+			return sqr(v.x - w.x) + sqr(v.y - w.y);
+		}
 
 		var l2 = dist2(v, w);
 		if (l2 == 0) return dist2(p, v);
 		var t = ((p.x - v.x) * (w.x - v.x) + (p.y - v.y) * (w.y - v.y)) / l2;
 		t = Math.max(0, Math.min(1, t));
-		return dist2(p, { x: v.x + t * (w.x - v.x),
-						y: v.y + t * (w.y - v.y) });
+		return dist2(p, {
+			x: v.x + t * (w.x - v.x),
+			y: v.y + t * (w.y - v.y)
+		});
 	}
 
 	// Sort the selected nodes based on x coordinate to get them in the right order
