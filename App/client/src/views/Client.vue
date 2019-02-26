c71fb049ab4e2a57c940f70009a227003e7f6db1
christofferholmesland <cr.holmesland@stud.uis.no>
2019-02-26 13:08:08 +0100

    wut

--- App/client/src/views/Client.vue
+++ App/client/src/views/Client.vue
@@ -12,7 +12,10 @@
     			<GraphDrawer 
 					@getValueResponse="printGraphDrawer" 
 					:requestAnswer="requestAnswer" 
-					controlType="Djikstra" />
+					controlType="Sort"
+					sortType="Quicksort"
+					:steps="steps"
+					operatingMode="Presentation" />
 				<b-button @click="requestAnswer = !requestAnswer" >Export</b-button>
 				</b-col>
 			</b-row>
@@ -28,7 +31,47 @@ export default {
 	name: "client",
 	data() {
 		return {
-			requestAnswer: false
+			requestAnswer: false,
+			steps: [{
+    "type": "Initial",
+    "list": [2344, 624, 62, 2347347, 3473, 47234, 0]
+}, {
+    "type": "Split",
+    "list": [2344, 624, 62, 2347347, 3473, 47234, 0],
+    "pivot": 2344,
+    "left": [624, 62, 0],
+    "right": [2347347, 3473, 47234]
+}, {
+    "type": "Split",
+    "list": [624, 62, 0],
+    "pivot": 62,
+    "left": [0],
+    "right": [624]
+}, {
+    "type": "Merge",
+    "leftSorted": [0],
+    "rightSorted": [624],
+    "pivot": 62,
+    "sorted": [0, 62, 624]
+}, {
+    "type": "Split",
+    "list": [2347347, 3473, 47234],
+    "pivot": 47234,
+    "left": [3473],
+    "right": [2347347]
+}, {
+    "type": "Merge",
+    "leftSorted": [3473],
+    "rightSorted": [2347347],
+    "pivot": 47234,
+    "sorted": [3473, 47234, 2347347]
+}, {
+    "type": "Merge",
+    "leftSorted": [0, 62, 624],
+    "rightSorted": [3473, 47234, 2347347],
+    "pivot": 2344,
+    "sorted": [0, 62, 624, 2344, 3473, 47234, 2347347]
+}]
 		};
 	},
 	components: {
