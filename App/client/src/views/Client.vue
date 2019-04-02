2832e14f530c62ee090c9e33f815456344a31c46
christofferholmesland <cr.holmesland@stud.uis.no>
2019-04-02 13:15:04 +0200

    Removed warnings from client.vue

--- App/client/src/views/Client.vue
+++ App/client/src/views/Client.vue
@@ -9,13 +9,10 @@
 				<b-col cols="12" lg="8">
 
 <!-- TODO: Remove GraphDrawer from this file. Currently used for testing -->
-    			<GraphDrawer 
-					@getValueResponse="printGraphDrawer" 
-					:requestAnswer="requestAnswer" 
+    			<GraphDrawer  
 					controlType="Python"
 					operatingMode="Interactive"
 				/>
-				<b-button @click="requestAnswer = !requestAnswer" >Export</b-button>
 <!--				<shellsort :initialList="[	]" :initialKValue="3"/> -->
 				</b-col>
 			</b-row>
