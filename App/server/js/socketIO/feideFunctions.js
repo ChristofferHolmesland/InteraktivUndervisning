990dc2e8471f0c9dc5d6865d6b107a68632eb7a6
GauteHaugen <gaute.haugen@lyse.net>
2019-04-04 15:39:17 +0200

    Fixed bug not displaying previous session for user

--- App/server/js/socketIO/feideFunctions.js
+++ App/server/js/socketIO/feideFunctions.js
@@ -20,7 +20,9 @@ module.exports.feide = function(socket, db, user){
         }
         */
 
-        dbFunctions.get.sessionsToUser(db, {id: user.feide.idNumber, type: "feide"}).then(async function(sessions) {
+        dbFunctions.get.sessionsToUser(
+            db, {id: user.feide.idNumber, type: "feide"}
+        ).then(async function(sessions) {
             result = {};
             if (sessions !== undefined){
                 result.sessionList = sessions;
@@ -54,6 +56,8 @@ module.exports.feide = function(socket, db, user){
                 console.error(err);
                 result.totalDidntKnowAnswers = "Not available at this time."
             });
+
+            console.log(result);
 
             socket.emit("getUserStatsResponse", result);
         }).catch((err) => {
