d696d140086a00612774ee84e0502c85bde191e9
GauteHaugen <gaute.haugen@lyse.net>
2019-05-11 09:39:23 +0200

    Fixed error not storing the answer to db

--- App/server/js/database/databaseInsertFunctions.js
+++ App/server/js/database/databaseInsertFunctions.js
@@ -67,17 +67,23 @@ const insert = {
 		return createPromise(db, statement, "storeAnswer");
 	},
 	storeAnswers: function(db, answerList, sqId) {
-		let statement = "BEGIN TRANSACTION;";
-		for (let i = 0; i < answerList.length; i++) {
-			let answer = answerList[i];
-			answer.answerObject = JSON.stringify(answer.answerObject);
-			answer.answerObject = answer.answerObject.replace("'", "\'\'");
-			statement += `INSERT INTO Answer(object, result, sessionHasQuestionId, userId)
-						Values('${answer.answerObject}', ${answer.result}, ${sqId}, '${answer.userId}');`;
-		}
-		statement += "COMMIT TRANSACTION;";
-		console.log(statement);
-		return createPromise(db, statement, "storeAnswers")
+		return new Promise((resolve, reject) => {
+			let statement = "BEGIN TRANSACTION;";
+			for (let i = 0; i < answerList.length; i++) {
+				let answer = answerList[i];
+				answer.answerObject = JSON.stringify(answer.answerObject);
+				answer.answerObject = answer.answerObject.replace("'", "\'\'");
+				statement += `INSERT INTO Answer(object, result, sessionHasQuestionId, userId)
+							Values('${answer.answerObject}', ${answer.result}, ${sqId}, '${answer.userId}');`;
+			}
+			statement += "COMMIT TRANSACTION;";
+			console.log(statement);
+	
+			db.exec(statement, (err) => {
+				if (err) reject(new Error(`Error in insert function: ${"storeAnswers"} \n\n ${err}`)	);
+				resolve();
+			});
+		});
 	},
 	addUserToSession: function(db, userId, sessionId) {
 		let statement = `INSERT INTO UserHasSession(userId,sessionId)
