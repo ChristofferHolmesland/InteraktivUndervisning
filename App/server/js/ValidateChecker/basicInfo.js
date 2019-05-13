e2fa28ff240ab2a9fc3d669e6eb109df674fbde9
GauteHaugen <gaute.haugen@lyse.net>
2019-05-13 15:12:32 +0200

    removed validation from description

--- App/server/js/ValidateChecker/basicInfo.js
+++ App/server/js/ValidateChecker/basicInfo.js
@@ -5,15 +5,10 @@ const check = function (solutionInfo) {
 	};
 
 	let title = String(solutionInfo.title);
-	let desc = String(solutionInfo.description);
 
 	if(title === undefined || title === "") {
 		result.passed = false;
 		result.errors.push("TitleIsEmptyError");
-	}
-	if(desc === undefined || desc === "") {
-		result.passed = false;
-		result.errors.push("DescriptionIsEmptyError");
 	}
 
 	return result;
